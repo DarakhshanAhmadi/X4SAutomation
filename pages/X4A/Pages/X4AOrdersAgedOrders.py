10844f3364ac72b19e16df7c4148881ebeb80f41
Neha K R <neha.kr@ingrammicro.com>
2023-07-04 19:09:30 +0530

    pagination code added

--- pages/X4A/Pages/X4AOrdersAgedOrders.py
+++ pages/X4A/Pages/X4AOrdersAgedOrders.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver.common.by import By
 
 from CommonUtilities.baseSet.BasePage import BasePage
@@ -13,13 +15,18 @@ class X4AAgedOrdersPage(BasePage):
     PAGINATION_NEXT_PAGE_ARROW = (By.XPATH, "//div[@class='MuiListItemButton-root MuiListItemButton-gutters MuiButtonBase-root css-1cl35i1-MuiButtonBase-root-MuiListItemButton-root']/*[@data-testid='NavigateNextIcon']")
     PAGINATION_PREVIOUS_PAGE_ARROW = (By.XPATH, "//div[@class='MuiListItemButton-root MuiListItemButton-gutters Mui-disabled MuiButtonBase-root Mui-disabled css-1u0qvcd-MuiButtonBase-root-MuiListItemButton-root']/*[@data-testid='NavigateBeforeIcon']")
     PAGINATION_TABS = (By.XPATH, "//div[@class='MuiListItemButton-root MuiListItemButton-gutters MuiButtonBase-root css-37enk1-MuiButtonBase-root-MuiListItemButton-root']")
+    TABLE_HEADER = (By.XPATH, "//div[@role='columnheader']")
 
     def go_to_aged_orders(self):
         try:
             self.do_click_by_locator(self.ORDER_MENU)
-            self.do_click_by_locator(self.AGED_ORDER_OPTION)
+            self.do_double_click(self.AGED_ORDER_OPTION)
             self.logger.info("Clicked on Aged orders in the menu.")
         except Exception as e:
-            self.logger.error('Exception occured while clicking on Aged orders ' + str(e))
+            self.logger.error('Exception occurred while clicking on Aged orders ' + str(e))
             raise e
+
+
+
+
 
