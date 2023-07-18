23f6cb9c6ddfbf83dd3d3382c9b475d09da1973f
USTIWS002 <shyam.tiwari@ingrammicro.com>
2023-07-18 13:24:32 +0530

    to resolve conflict
    to resolve conflict

--- db/DbScript.sql
+++ db/DbScript.sql
@@ -14,12 +14,3 @@ reseller_po String,
 vendor_name String,
 order_status String
 );
-
-CREATE TABLE IF NOT EXISTS x4a_user_data(
-id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-feature_file_name String NOT NULL,
-Associate_Name String,
-Associate_Email	String,
-Associate_Roles	String,
-Associate_countries String
-);
