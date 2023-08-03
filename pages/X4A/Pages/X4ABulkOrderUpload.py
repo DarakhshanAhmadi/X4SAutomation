7d1037d814f9f61fbac580219ab400e5b27bc043
USTIWS002 <shyam.tiwari@ingrammicro.com>
2023-08-03 18:18:55 +0530

    Test Cases for EDT-8252
    Test case for
    EDT-8751
    EDT-8842
    EDT-8843
    EDT-8844
    EDT-8845

---
+++ pages/X4A/Pages/X4ABulkOrderUpload.py
@@ -0,0 +1,192 @@
+from selenium.webdriver.common.by import By
+
+from CommonUtilities import readWriteTestData
+from CommonUtilities.baseSet.BasePage import BasePage
+from CommonUtilities.parse_config import ParseConfigFile
+from CommonUtilities.readProperties import ReadConfig, parse_config_file
+from pynput.keyboard import Key, Controller
+# from pynput.mouse import Controller, Button
+import time
+
+class X4ABulkOrderUploadPage(BasePage):
+    parse_config_json = ParseConfigFile()
+    screen_shot_path = ReadConfig.getScreenshotPath()
+    file_path = ReadConfig.get_file_path()
+
+    ORDER_MENU = (By.XPATH, "//*[@data-testid='SalesIcon']")
+    BULK_ORDER_UPLOAD_OPTION = (By.XPATH, "//*[text()='Bulk order upload ']")
+    BULK_ORDER_UPLOAD_TEXT = (By.XPATH, "//h3[text()='Bulk order upload']")
+    UPLOAD_FILE_BUTTON = (By.XPATH, "//button[text()='Upload file']")
+    UPLOAD_FILE_TITLE = (By.XPATH, "//span[text()='Upload file']")
+    DOWNLOAD_TEMPLATE_BUTTON = (By.XPATH, "//button[text()='Download template']")
+    DISABLED_REVIEW_BUTTON = (By.XPATH, "//button[@disabled and text() = 'Review']")
+    DISABLED_PLACE_ORDER_BUTTON = (By.XPATH, "//button[@disabled and text() = 'Place orders']")
+    REVIEW_BUTTON = (By.XPATH, "//button[text() = 'Review']")
+    PLACE_ORDER_BUTTON = (By.XPATH, "//button[text() = 'Place orders']")
+    CANCEL_BUTTON = (By.XPATH, "//button[text() = 'Cancel']")
+    CLOSE_ICON = (By.XPATH, "//*[@data-testid='CloseIcon']")
+    BROWSE_BUTTON = (By.XPATH, '//p[text()="Browse"]')
+    ENABLED_CANCEL_BUTTON = (By.XPATH, "//button[not(@disable) and text() = 'Cancel']")
+    UPLOAD_FILE_LABEL = (By.XPATH, "//div[@class='MuiBox-root css-zdp87v']")
+    CHECKBOX = (By.XPATH, "//div[@data-rowindex='2']/div[@data-field='__check__']")
+    SEARCH_BOX = (By.ID, "search")
+    SEARCH_BOX_SEARCH_ICON = (By.XPATH, "//*[@data-testid='SearchIcon']")
+    FILE_NAME = (By.XPATH, "//div[@class='MuiBox-root css-1oulfbi']")
+    DELETE_ICON = (By.XPATH, "//*[@data-testid='DeleteOutlineIcon']")
+    REVIEW_BUTTON = (By.XPATH, "//button[text() = 'Review']")
+    PLACE_ORDER_BUTTON = (By.XPATH, "//button[text() = 'Place orders']")
+    FILE_ERROR_MSG = (By.XPATH, "//div[@class='MuiAlert-message css-acap47-MuiAlert-message']")
+    TEMPLATE_ERROR_MSG = (By.XPATH, "//div[@class='MuiAlert-message css-acap47-MuiAlert-message']")
+    BACK_BUTTON = (By.XPATH, "//button[text() = 'Back']")
+    USER_DROPDOWN = (By.XPATH, "//*[@data-testid='KeyboardArrowDownIcon']")
+    LOGOUT = (By.XPATH, "//*[text()='LogOut']")
+
+    def go_to_bulk_order_upload(self):
+        try:
+            self.do_click_by_locator(self.ORDER_MENU)
+            self.logger.info("Clicked on Order in the menu")
+            self.do_double_click(self.BULK_ORDER_UPLOAD_OPTION)
+            self.logger.info("Clicked on Bulk Order Upload option")
+
+        except Exception as e:
+            self.logger.error('Exception occurred while clicking on Bulk Order Upload ' + str(e))
+            raise e
+
+    def verify_bulk_order_upload_page(self):
+        try:
+            assert 'Bulk order upload' in self.get_element_text(self.BULK_ORDER_UPLOAD_TEXT),"Bulk Order Upload Title not present"
+            assert 'Upload file' in self.get_element_text(self.UPLOAD_FILE_BUTTON),"Bulk Order Upload Title not present"
+            assert 'Download template' in self.get_element_text(self.DOWNLOAD_TEMPLATE_BUTTON),"Bulk Order Upload Title not present"
+            self.logger.info("verified that bulk order upload page")
+
+        except Exception as e:
+            self.logger.error('Exception occurred while verifying bulk order upload page ' + str(e))
+            raise e
+
+    def click_upload_file(self):
+        try:
+            self.do_click_by_locator(self.UPLOAD_FILE_BUTTON)
+            self.logger.info("Clicked on upload file button")
+
+        except Exception as e:
+            self.logger.error('Exception occurred while clicking upload file button ' + str(e))
+            raise e
+
+    def verify_upload_file_popup(self):
+        try:
+            upload_file_label_msg = 'Drag and Drop file here or Browse files from your computer Supported file type: Excel (.xls, .xlsx)'
+            address = self.get_element_text(self.UPLOAD_FILE_LABEL).replace("\n", " ")
+            assert 'Review' in self.get_element_text(self.DISABLED_REVIEW_BUTTON), "Disabled Review button not present"
+            assert 'Place orders' in self.get_element_text(self.DISABLED_PLACE_ORDER_BUTTON), "Disabled Place Order button not present"
+            assert 'Cancel' in self.get_element_text(self.CANCEL_BUTTON), "Cancel button not present"
+            assert address in upload_file_label_msg, "label message not present"
+            self.logger.info("verified that upload file popup")
+            time.sleep(5)
+            # self.do_click_by_locator(self.CANCEL_BUTTON)
+
+        except Exception as e:
+            self.logger.error('Exception occurred while verifying cancel order popup is closed ' + str(e))
+            raise e
+
+    def do_upload_error_file(self):
+        try:
+            self.do_click_by_locator(self.BROWSE_BUTTON)
+            time.sleep(3)
+            keyboard = Controller()
+            keyboard.type(self.file_path + 'BulkOrderErrorFile.txt')
+            keyboard.press(Key.enter)
+            keyboard.release(Key.enter)
+            time.sleep(5)
+
+        except Exception as e:
+            self.logger.error('Exception occurred while selecting file ' + str(e))
+            raise e
+    def verify_error_popup(self):
+        try:
+            file_error_msg = 'File type not supported please try again by uploading an Excel file'
+            address = self.get_element_text(self.FILE_ERROR_MSG).replace("\n", " ")
+
+            assert file_error_msg in address,"Bulk Order Upload file error message not present"
+            self.logger.info("verified that bulk order upload file error")
+            time.sleep(5)
+
+        except Exception as e:
+            self.logger.error('Exception occurred while verifying bulk order upload file error ' + str(e))
+            raise e
+
+    def do_upload_template_error_file(self):
+        try:
+            self.do_click_by_locator(self.BROWSE_BUTTON)
+            time.sleep(3)
+            keyboard = Controller()
+            keyboard.type(self.file_path + 'Input_File.xlsx')
+            keyboard.press(Key.enter)
+            keyboard.release(Key.enter)
+            time.sleep(1)
+
+        except Exception as e:
+            self.logger.error('Exception occurred while selecting file ' + str(e))
+            raise e
+    def verify_template_error_message(self):
+        try:
+            template_error_msg = 'Template mismatch The provided file contains not supported data by the IM standard template, mandatory colums are missing.'
+            address = self.get_element_text(self.TEMPLATE_ERROR_MSG).replace("\n", " ")
+            assert template_error_msg in address,"Bulk Order Upload file template error message not present"
+            assert self.is_present(self.DELETE_ICON), "Delete Icon not found"
+            self.logger.info("verified that bulk order upload template file error")
+            self.do_click_by_locator(self.DELETE_ICON)
+            time.sleep(1)
+
+        except Exception as e:
+            self.logger.error('Exception occurred while verifying bulk order upload file error ' + str(e))
+            raise e
+
+    def do_select_file(self):
+        try:
+            bulk_order_file_path = parse_config_file.get_data_from_config_json("inputFile", "bulkOrderInputFileName")
+            self.do_click_by_locator(self.BROWSE_BUTTON)
+            time.sleep(3)
+            keyboard = Controller()
+            keyboard.type(self.file_path + bulk_order_file_path)
+            keyboard.press(Key.enter)
+            keyboard.release(Key.enter)
+            time.sleep(1)
+
+        except Exception as e:
+            self.logger.error('Exception occurred while selecting file ' + str(e))
+            raise e
+
+    def verify_selected_file_popup(self):
+        try:
+            upload_file_label_msg = 'Your file is ready to be processed to place orders. Testdata_1_QA.xlsx 9.27 KB'
+            address = self.get_element_text(self.UPLOAD_FILE_LABEL).replace("\n", " ")
+            assert 'Review' in self.get_element_text(self.REVIEW_BUTTON), "Disabled Review button not present"
+            assert 'Place orders' in self.get_element_text(self.PLACE_ORDER_BUTTON), "Disabled Place Order button not present"
+            assert 'Cancel' in self.get_element_text(self.CANCEL_BUTTON), "Cancel button not present"
+            assert self.is_present(self.DELETE_ICON), "Delete Icon not found"
+            assert address in upload_file_label_msg, "label message not present"
+            self.logger.info("verified that upload file popup")
+            time.sleep(1)
+
+        except Exception as e:
+            self.logger.error('Exception occurred while verifying cancel order popup is closed ' + str(e))
+            raise e
+
+    def do_delete_file(self):
+        try:
+            self.do_click_by_locator(self.DELETE_ICON)
+            time.sleep(1)
+
+        except Exception as e:
+            self.logger.error('Exception occurred while selecting file ' + str(e))
+            raise e
+
+    def logout_x4a(self):
+        try:
+            self.do_click_by_locator(self.USER_DROPDOWN)
+            self.do_click_by_locator(self.LOGOUT)
+            self.logger.info("Logout Successfully")
+            return True
+        except Exception as e:
+            self.logger.error('Exception occurred while Logout X4A ' + str(e))
+            return False
---
+++ tests/step_defs/hardware/test_bulk_order_update.py
@@ -0,0 +1,216 @@
+from pytest_bdd import scenario, parsers, when, then, given
+from CommonUtilities import readWriteTestData
+from CommonUtilities.parse_config import ParseConfigFile
+from CommonUtilities.file_operations import logger
+from CommonUtilities.readProperties import ReadConfig
+from db.service.X4AInputOrderDbManagementService import X4AInputOrderDbManagementService
+from pages.X4A.Facade.PrepareObject import PrepareObject
+from pages.X4A.TestSteps.validateBulkOrderUploadData import ValidateBulkOrderUploadData
+
+parse_config_json = ParseConfigFile()
+screen_shot = {"path": " "}
+x4a_status_list = []
+x4a_input_order_list = []
+db_file_path = ReadConfig.get_db_file_path()
+
+@scenario("features/hardware/bulk_order_upload.feature", "Login to X4A portal")
+def test_login_the_x4a_portal():
+    pass
+
+@scenario("features/hardware/bulk_order_upload.feature", "File Upload option & Dialog Box")
+def test_upload_option():
+    pass
+#
+@scenario("features/hardware/bulk_order_upload.feature", "Upload file error")
+def test_upload_file_error():
+    pass
+
+@scenario("features/hardware/bulk_order_upload.feature", "Template error")
+def test_template_error():
+    pass
+
+@scenario("features/hardware/bulk_order_upload.feature", "Upload file")
+def test_upload_file():
+    pass
+
+
+@scenario("features/hardware/bulk_order_upload.feature", "logout X4A")
+def test_logout_x4a():
+    pass
+
+
+@given(parsers.parse('launch chrome browser and open the X4A url'))
+def launch_browser(init_driver):
+    bulk_order_upload_steps = ValidateBulkOrderUploadData(init_driver)
+    order_management_srv_obj = X4AInputOrderDbManagementService()
+    prepare_obj = PrepareObject(init_driver)
+    feature_file_name = "bulk_order_upload"
+    try:
+        test_data_order = readWriteTestData.load_excel_to_dictionary(ReadConfig.get_test_data_file(), "Input_Data")
+        filtered_order_data = bulk_order_upload_steps.filtered_orders_by_feature_file(test_data_order, feature_file_name)
+        logger.info(filtered_order_data)
+        for order_index, test_data_order in filtered_order_data.iterrows():
+            x4a_input_order_list.clear()
+            logger.info(test_data_order)
+            x4a_input_order_data = prepare_obj.prepare_x4a_inp_ord_data_obj(test_data_order)
+            x4a_input_order_list.append(x4a_input_order_data)
+            order_management_srv_obj.save_x4a_input_order(db_file_path, x4a_input_order_list)
+        environment = parse_config_json.get_data_from_config_json("environment", "environment_type", "config.json")
+        logger.info(environment)
+        if environment == 'Stage':
+            url = parse_config_json.get_data_from_config_json("x4aStageCredentials", "x4aBaseUrl", "config.json")
+        else:
+            url = parse_config_json.get_data_from_config_json("x4aBetaCredentials", "x4aBaseUrl", "config.json")
+        init_driver.get(url)
+    except Exception as e:
+        logger.error("Not able to Launch the browser and login x4a %s", e)
+        raise e
+
+
+@then(parsers.parse('provide user ID and Password to login'))
+def login(init_driver):
+    feature_file_name = "bulk_order_upload"
+    bulk_order_upload_steps = ValidateBulkOrderUploadData(init_driver)
+    try:
+        bulk_order_upload_steps.login(feature_file_name, screen_shot)
+        logger.info("Launched the browser and login to X4A is successfully.")
+    except Exception as e:
+        logger.error("Not able to Launch the browser and login x4a %s", e)
+        raise e
+
+
+@when(parsers.parse('the user traverse to Bulk order update menu'))
+def click_on_bulk_order_upload_menu(init_driver):
+    feature_file_name = "bulk_order_upload"
+    bulk_order_upload_steps = ValidateBulkOrderUploadData(init_driver)
+    try:
+        if not bulk_order_upload_steps.click_on_bulk_order_upload_menu(feature_file_name, screen_shot):
+            raise Exception("Failed to click on bulk order upload menu")
+    except Exception as e:
+        logger.error("Error while clicking on bulk order upload menu %s", e)
+        raise e
+
+
+@then(parsers.parse('verify bulk order update page'))
+def verify_bulk_order_upload_page(init_driver):
+    feature_file_name = "bulk_order_upload"
+    bulk_order_upload_steps = ValidateBulkOrderUploadData(init_driver)
+    try:
+        if not bulk_order_upload_steps.verify_bulk_order_upload_page(feature_file_name, screen_shot):
+            raise Exception("Failed to verify bulk order upload page")
+    except Exception as e:
+        logger.error("Error while verifying bulk order upload page %s", e)
+        raise e
+
+@when(parsers.parse('upload file button clicked'))
+def click_upload_file(init_driver):
+    feature_file_name = "bulk_order_upload"
+    bulk_order_upload_steps = ValidateBulkOrderUploadData(init_driver)
+    try:
+        if not bulk_order_upload_steps.click_upload_file(feature_file_name, screen_shot):
+            raise Exception("Failed to click upload file button")
+    except Exception as e:
+        logger.error("Error while clicking upload file button %s", e)
+        raise e
+
+@then(parsers.parse('verify upload file popup'))
+def verify_upload_file_popup(init_driver):
+    feature_file_name = "bulk_order_upload"
+    bulk_order_upload_steps = ValidateBulkOrderUploadData(init_driver)
+    try:
+        if not bulk_order_upload_steps.verify_upload_file_popup(feature_file_name, screen_shot):
+            raise Exception("Failed to verify upload file popup")
+    except Exception as e:
+        logger.error("Error while verifying upload file popup %s", e)
+        raise e
+
+
+@when(parsers.parse('other than excel file was uploaded'))
+def do_upload_error_file(init_driver):
+    feature_file_name = "bulk_order_upload"
+    bulk_order_upload_steps = ValidateBulkOrderUploadData(init_driver)
+    try:
+        if not bulk_order_upload_steps.do_upload_error_file(feature_file_name, screen_shot):
+            raise Exception("Failed to select error file")
+    except Exception as e:
+        logger.error("Error while selecting file %s", e)
+        raise e
+
+@then(parsers.parse('verify error message'))
+def verify_error_popup(init_driver):
+    feature_file_name = "bulk_order_upload"
+    bulk_order_upload_steps = ValidateBulkOrderUploadData(init_driver)
+    try:
+        if not bulk_order_upload_steps.verify_error_popup(feature_file_name, screen_shot):
+            raise Exception("Failed to verify file error popup")
+    except Exception as e:
+        logger.error("Error while verifying file error popup %s", e)
+        raise e
+
+
+@when(parsers.parse('different template was uploaded'))
+def do_upload_template_error_file(init_driver):
+    feature_file_name = "bulk_order_upload"
+    bulk_order_upload_steps = ValidateBulkOrderUploadData(init_driver)
+    try:
+        if not bulk_order_upload_steps.do_upload_template_error_file(feature_file_name, screen_shot):
+            raise Exception("Failed to select error file")
+    except Exception as e:
+        logger.error("Error while selecting file %s", e)
+        raise e
+
+@then(parsers.parse('verify template error message'))
+def verify_template_error_message(init_driver):
+    feature_file_name = "bulk_order_upload"
+    bulk_order_upload_steps = ValidateBulkOrderUploadData(init_driver)
+    try:
+        if not bulk_order_upload_steps.verify_template_error_message(feature_file_name, screen_shot):
+            raise Exception("Failed to verify template error message")
+    except Exception as e:
+        logger.error("Error while verifying template error message %s", e)
+        raise e
+
+@when(parsers.parse('file was uploaded'))
+def do_upload_file(init_driver):
+    feature_file_name = "bulk_order_upload"
+    bulk_order_upload_steps = ValidateBulkOrderUploadData(init_driver)
+    try:
+        if not bulk_order_upload_steps.do_select_file(feature_file_name, screen_shot):
+            raise Exception("Failed to select file")
+    except Exception as e:
+        logger.error("Error while selecting file %s", e)
+        raise e
+
+@then(parsers.parse('verify upload file ready popup'))
+def verify_selected_file_popup(init_driver):
+    feature_file_name = "bulk_order_upload"
+    bulk_order_upload_steps = ValidateBulkOrderUploadData(init_driver)
+    try:
+        if not bulk_order_upload_steps.verify_selected_file_popup(feature_file_name, screen_shot):
+            raise Exception("Failed to verify selected file popup")
+    except Exception as e:
+        logger.error("Error while verifying selecting file popup %s", e)
+        raise e
+
+@when(parsers.parse('click on delete icon'))
+def do_delete_file(init_driver):
+    feature_file_name = "bulk_order_upload"
+    bulk_order_upload_steps = ValidateBulkOrderUploadData(init_driver)
+    try:
+        if not bulk_order_upload_steps.do_delete_file(feature_file_name, screen_shot):
+            raise Exception("Failed to delete file")
+    except Exception as e:
+        logger.error("Error while deleting file %s", e)
+        raise e
+
+
+@given(parsers.parse('logout the X4A url'))
+def logout_x4a_url(init_driver):
+    feature_file_name = "bulk_order_upload"
+    bulk_order_upload_steps = ValidateBulkOrderUploadData(init_driver)
+    try:
+        bulk_order_upload_steps.logout_x4a_url(feature_file_name)
+        logger.info("Logout X4A url is successfully.")
+    except Exception as e:
+        logger.error("Not able to logout x4a url %s", e)
+        raise e
