4d60fe86414e8b42853fcc5dadfba7637b57125d
Neha K R <neha.kr@ingrammicro.com>
2023-10-10 18:57:18 +0530

    OMS-52, OMS-53 testcases added

---
+++ db/model/X4AInventory.py
@@ -0,0 +1,8 @@
+class X4AInventory:
+    def __init__(self, feature_file_name, sku, mfn_part_number, vendor_business_manager, vendor_name, country):
+        self.feature_file_name = feature_file_name
+        self.sku = sku
+        self.mfn_part_number = mfn_part_number
+        self.vendor_business_manager = vendor_business_manager
+        self.vendor_name = vendor_name
+        self.country = country
